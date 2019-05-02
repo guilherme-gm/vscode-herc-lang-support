daf5bfbd2868de3c3d6f8bc4bc9a14b38fd23976
Guilherme Menaldo <guilherme.menaldo@outlook.com>
2019-05-02 11:45:39 -0300

    Removed old test file leftover

--- server/src/parser/parserTest.ts
+++
@@ -1,8 +0,0 @@
-import * as parser from "./parser";
-//1,9
-//1,11
-console.log("------------- AT 1,9 -------------");
-console.log(parser.getContextOfText("prontera,150,150,4	script	Test	FAKE_NPC,{\n\tgetitem()\n}", { line: 1, character: 9 }));
-
-console.log("------------- AT 1,13 -------------");
-console.log(parser.getContextOfText("prontera,150,150,4	script	Test	FAKE_NPC,{\n\tgetitem(501,)\n}", { line: 1, character: 13 }));
